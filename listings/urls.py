01f78f6b0ab16895f68fd5befb58d74e59f3b233
arvind chaudhary <chaudhary003@gmail.com>
2020-05-02 16:23:20 +0530

    featured listing and pagination

--- listings/urls.py
+++ listings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
             path('',views.listings,name='listings'),
-            path('<int:id>',views.listing,name='listing')
+            path('<int:id>',views.listing,name='listing'),
+            path('<title>',views.listing_by_title,name='listing_by_title')
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Listing
+from django.core.paginator import Paginator
 # Create your views here.
 def listings(request):
-    return render(request,'listings/listings.html')
+    listings=Listing.objects.all()
+    paginator=Paginator(listings,3)
+    page=request.GET.get('page')
+    listing_per_page=paginator.get_page(page)
+    context={
+    'listings':listing_per_page
+    }
+    return render(request,'listings/listings.html',context)
 def listing(request,id):
     listing=get_object_or_404(Listing,pk=id)
+    context={
+    'listing':listing
+    }
+
+    return render(request,'listings/listing.html',context)
+def listing_by_title(request,title):
+    listing=get_object_or_404(Listing,title=title)
     context={
     'listing':listing
     }
